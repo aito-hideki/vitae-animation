36341c1d8a40d14f7ee82d386026c6a472a619d1
Aito Hideki <aito.hideki@gmail.com>
2022-11-24 03:48:52 -0500

    style(floating text): updated floating text opacity transition

--- src/components/floating-text.vue
+++ src/components/floating-text.vue
@@ -20,10 +20,7 @@ const props = defineProps<{
 }>()
 
 const { scrollPos } = usePages()
-const textOpacity = computed(() => {
-  const opacity = props.posStart - scrollPos.value * 2
-  return opacity < 1 ? opacity * 2 : 0
-})
+const textOpacity = computed(() => props.posStart - scrollPos.value * 2)
 const paddingBottom = computed(() => textOpacity.value * 160 + 'px')
 </script>
 
