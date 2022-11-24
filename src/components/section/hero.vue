0fe37e6f8e0d748f2c0d8df6d46879e050befa15
Aito Hideki <aito.hideki@gmail.com>
2022-11-23 22:09:27 -0500

    feat(hero): hero section added

--- src/components/section/hero.vue
+++ src/components/section/hero.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="h-screen">
-    Hero section
+  <div class="container h-screen flex flex-col justify-end px-5 mx-auto">
+    <div class="text-5xl sm:text-8xl lg:text-9xl xl:text-10xl mb-2">
+      Sunlight,<br>
+      re-imagined.
+    </div>
+    <div class="mb-3">
+      Circadian rhythmn aligned lighting by Vitae.
+    </div>
+    <arrow-down class="h-38 -mb-8" />
   </div>
 </template>
