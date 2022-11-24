e6d0b7adcb36a3d3882e6b8123119f458deffc26
Aito Hideki <aito.hideki@gmail.com>
2022-11-23 22:37:35 -0500

    style(light bulb): light bulb color transition

--- src/components/sunshine-backs.vue
+++ src/components/sunshine-backs.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="opacity-70">
-    <div class="overlay-ellipse smaller-ellipse bg-dull-yellow" />
-    <div class="overlay-ellipse larger-ellipse bg-yellow" />
+    <div
+      class="overlay-ellipse smaller-ellipse"
+      :class="page < 4 ? 'bg-dull-yellow' : 'white'"
+    />
+    <div
+      class="overlay-ellipse larger-ellipse"
+      :class="page < 4 ? 'bg-yellow' : 'white'"
+    />
   </div>
 </template>
+
+<script setup lang="ts">
+import { usePages } from '@/use/pages'
+
+const { page } = usePages()
+</script>
