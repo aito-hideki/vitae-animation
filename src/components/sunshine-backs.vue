5f58b485328db062ca8b58c7d89830f734de2e58
Aito Hideki <aito.hideki@gmail.com>
2022-11-23 22:46:24 -0500

    style(overlay): overlay effect transition

--- src/components/sunshine-backs.vue
+++ src/components/sunshine-backs.vue
@@ -1,14 +1,25 @@
 <template>
-  <div class="opacity-70">
+  <transition>
     <div
-      class="overlay-ellipse smaller-ellipse"
-      :class="page < 4 ? 'bg-dull-yellow' : 'white'"
-    />
+      v-if="page === 1"
+      class="opacity-70"
+    >
+      <div
+        class="overlay-ellipse smaller-ellipse"
+        :class="page < 4 ? 'bg-dull-yellow' : 'white'"
+      />
+      <div
+        class="overlay-ellipse larger-ellipse"
+        :class="page < 4 ? 'bg-yellow' : 'white'"
+      />
+    </div>
     <div
-      class="overlay-ellipse larger-ellipse"
-      :class="page < 4 ? 'bg-yellow' : 'white'"
-    />
-  </div>
+      v-else-if="page === 2"
+      class="bg-orange"
+    >
+      <div class="fixed-screen gradient-overlay" />
+    </div>
+  </transition>
 </template>
 
 <script setup lang="ts">
@@ -16,3 +27,9 @@ import { usePages } from '@/use/pages'
 
 const { page } = usePages()
 </script>
+
+<style scoped>
+.gradient-overlay {
+  background: radial-gradient(66.36% 66.36% at 50% 33.64%, rgba(22, 21, 31, 0) 0%, #4A1307 100%);
+}
+</style>
