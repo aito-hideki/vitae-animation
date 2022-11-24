2d71cc03ed814bf0e834d5639aca939595e8b224
Aito Hideki <aito.hideki@gmail.com>
2022-11-23 22:33:04 -0500

    style(background): background color transition

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="bg-sun">
+  <div
+    class="duration-300"
+    :class="colorClass"
+  >
     <sunshine-backs />
     <div class="mix-blend-overlay">
       <sunshine />
@@ -9,7 +12,31 @@
     <div class="relative">
       <section-hero />
       <section-texts />
+      <div
+        v-for="i in 3"
+        :key="i"
+        class="h-screen"
+      />
     </div>
     <pagination />
   </div>
 </template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { usePages } from './use/pages'
+
+const { page } = usePages()
+const colorClass = computed(() => {
+  switch (page.value) {
+    case 1:
+      return 'bg-sun'
+    case 2:
+      return 'bg-orange'
+    case 3:
+      return 'bg-warm-white'
+    default:
+      return 'bg-daylight-white'
+  }
+})
+</script>
